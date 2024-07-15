ec4de97618361635b309714f94b11b35751864fa
DzungVu <dungvu0392@gmail.com>
2024-07-15 20:08:17 +0700

    init

--- src/test/java/feature/Login2Test.java
+++ src/test/java/feature/Login2Test.java
@@ -9,7 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Login1Test {
+public class Login2Test {
     public WebDriver driver;
 
     @BeforeClass
