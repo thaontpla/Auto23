fcf805fc1c17947e7a5fc554237f3e2dd4b4188f
thaontpla <thaontp.la@gmail.com>
2024-07-15 19:46:39 +0700

    addition test case

---
+++ src/test/java/feature/LoginVersionTest.java
@@ -0,0 +1,54 @@
+package feature;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginVersionTest {
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        // Thiết lập WebDriver (trình duyệt Chrome)
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testLogin() throws InterruptedException {
+        // Mở trang web
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(5000);
+
+        // Nhập thông tin đăng nhập
+        WebElement usernameInput = driver.findElement(By.id("user-name"));
+        usernameInput.sendKeys("standard_user");
+        Thread.sleep(5000);
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("secret_sauce");
+        Thread.sleep(5000);
+
+        // Click vào nút đăng nhập
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        Thread.sleep(5000);
+
+        // Kiểm tra xem đăng nhập thành công hay không bằng cách kiểm tra URL mới
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, "https://www.saucedemo.com/inventory.html");
+    }
+
+
+
+    @AfterClass
+    public void tearDown() {
+        // Đóng trình duyệt
+        driver.quit();
+    }
+}
